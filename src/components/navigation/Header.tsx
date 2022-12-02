fbf20f0db9cbdf8f17d892ceb68375b985f188ac
Mathewooo <matej.sablica@gmail.com>
2022-12-02 14:59:52 +0100

    Update

--- src/components/navigation/Header.tsx
+++ src/components/navigation/Header.tsx
@@ -1,5 +1,5 @@
 import type {Component} from 'solid-js'
-import {createSignal, Show, For} from 'solid-js'
+import {createSignal, Show, For, JSX} from 'solid-js'
 import {A} from "@solidjs/router"
 import './styles/Header.sass'
 
@@ -11,7 +11,7 @@ const LINKS: [string, string][] = [
     ["/projects", "Projects"],
 ]
 
-const pixelsUntilChange: number = 650;
+const pixelsUntilChange: number = 700
 
 const App: Component = () => {
     const checkWindowSize = (): boolean => {
@@ -25,46 +25,68 @@ const App: Component = () => {
     )
 
     onresize = () => {
-        setCanChange(
+        const isMobile: boolean =
             checkWindowSize()
+        setCanChange(
+            isMobile
         )
-    };
+        if (active() && !isMobile) {
+            setActive(!active())
+        }
+    }
 
     const handleMenu = () => setActive(
         !active()
     )
 
+    function returnLinks(
+        mobile: boolean
+    ): JSX.Element {
+        return (
+            <ul classList={{
+                list: !mobile,
+                mobileList: mobile, listAnim: mobile
+            }}>
+                <For each={LINKS}>
+                    {(link) => (
+                        <li>
+                            <A href={link[0]}>{
+                                link[1]
+                            }</A>
+                        </li>
+                    )}
+                </For>
+            </ul>
+        )
+    }
+
     return (
         <header class='navigation'>
-            <h1>{TITLE.toUpperCase()}</h1>
-            <nav>
-                <ul classList={{
-                    mobileList: canChange(),
-                    list: !canChange()
-                }}>
-                    <For each={LINKS}>
-                        {(link) => (
-                            <li>
-                                <A href={link[0]}>{
-                                    link[1]
-                                }</A>
-                            </li>
-                        )}
-                    </For>
-                </ul>
-                <Show when={canChange()} keyed>
-                    <div classList={{
-                        active: active(),
-                        menu: true
-                    }} onClick={handleMenu}>
-                        <div>
+            <section>
+                <h1>{TITLE.toUpperCase()}</h1>
+                <nav>
+                    <Show when={
+                        !canChange()
+                    } keyed>
+                        {returnLinks(false)}
+                    </Show>
+                    <Show when={canChange()} keyed>
+                        <div classList={{
+                            active: active(),
+                            menu: true
+                        }} onClick={handleMenu}>
                             <span/>
                         </div>
-                    </div>
-                </Show>
-            </nav>
+                    </Show>
+                </nav>
+            </section>
+            <Show when={
+                canChange() && active()
+            } keyed>
+                {returnLinks(true)}
+            </Show>
         </header>
-    );
-};
+    )
+}
 
 export default App;
